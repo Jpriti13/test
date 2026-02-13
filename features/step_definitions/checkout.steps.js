73708c2315ffd0da41a2ce6a280b93940b00f817
Jpriti13 <155743371+Jpriti13@users.noreply.github.com>
2026-02-13 17:39:35 +0530

    Add checkout step definitions for the checkout feature

---
+++ features/step_definitions/checkout.steps.js
@@ -0,0 +1,21 @@
+// Checkout step definitions
+
+Given('I am on the checkout page', function () {
+    // code to navigate to checkout page
+});
+
+When('I enter the shipping information', function (data) {
+    // code to enter shipping information
+});
+
+When('I enter the payment information', function (data) {
+    // code to enter payment information
+});
+
+Then('I should see the order confirmation', function () {
+    // code to verify order confirmation
+});
+
+Then('I should see an error message', function () {
+    // code to verify error message
+});
