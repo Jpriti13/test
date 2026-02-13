0c35afbabf833839a0c694ef4bb338e12cadcc7a
Jpriti13 <155743371+Jpriti13@users.noreply.github.com>
2026-02-13 17:39:47 +0530

    Add step definitions for product feature

---
+++ features/step_definitions/product.steps.js
@@ -0,0 +1,13 @@
+// Step definitions for product feature
+
+Given('I have a product with name {string}', (name) => {
+    // Implementation here
+});
+
+When('I add the product to the cart', () => {
+    // Implementation here
+});
+
+Then('I should see the product in the cart', () => {
+    // Implementation here
+});
